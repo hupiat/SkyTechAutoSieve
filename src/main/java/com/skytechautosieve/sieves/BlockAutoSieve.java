f87328ea59dcd2573d96b2a4386efb775cc1bac4
Hugo Piat <h.piat@outlook.fr>
2025-01-23 15:58:20 +0100

    fixed sift machine textures when placed

--- src/main/java/com/skytechautosieve/sieves/BlockAutoSieve.java
+++ src/main/java/com/skytechautosieve/sieves/BlockAutoSieve.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
@@ -17,13 +16,23 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class BlockAutoSieve extends BlockContainer {
+public class BlockAutoSieve extends Block {
 
 	public BlockAutoSieve() {
 		super(Material.GRASS);
 		setTranslationKey("auto_sieve");
 		setRegistryName("auto_sieve");
 		setCreativeTab(CreativeTabs.MISC);
+	}
+
+	@Override
+	public boolean hasTileEntity(IBlockState state) {
+		return true;
+	}
+
+	@Override
+	public TileEntity createTileEntity(World world, IBlockState state) {
+		return new TileEntityAutoSieve();
 	}
 
 	@Override
@@ -58,10 +67,5 @@ public class BlockAutoSieve extends BlockContainer {
 			return true;
 		}
 		return false;
-	}
-
-	@Override
-	public TileEntity createNewTileEntity(World worldIn, int meta) {
-		return new TileEntityAutoSieve();
 	}
 }
