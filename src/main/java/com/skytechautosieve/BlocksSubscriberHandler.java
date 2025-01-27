457a3dcc8108d7525f5746d18dfbf6497c8946ec
Hugo Piat <h.piat@outlook.fr>
2025-01-27 21:53:47 +0100

    Fixed sift machine visual, matching now with exAstris

--- src/main/java/com/skytechautosieve/BlocksSubscriberHandler.java
+++ src/main/java/com/skytechautosieve/BlocksSubscriberHandler.java
@@ -2,6 +2,7 @@ package com.skytechautosieve;
 
 import com.skytechautosieve.items.ItemFortuneUpgrade;
 import com.skytechautosieve.items.ItemSpeedUpgrade;
+import com.skytechautosieve.sieves.BakedModelAutoSieve;
 import com.skytechautosieve.sieves.BlockAutoSieve;
 import com.skytechautosieve.sieves.RenderBlockAutoSieve;
 import com.skytechautosieve.sieves.RenderItemAutoSieve;
@@ -11,6 +12,7 @@ import net.minecraft.block.Block;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraftforge.client.event.ModelBakeEvent;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
@@ -27,6 +29,16 @@ public class BlocksSubscriberHandler {
 	public static final Item SPEED_UPGRADE = new ItemSpeedUpgrade();
 	public static final Item FORTUNE_UPGRADE = new ItemFortuneUpgrade();
 
+	static {
+		AUTO_SIEVE_ITEM.setTileEntityItemStackRenderer(new RenderItemAutoSieve());
+	}
+
+	@SubscribeEvent
+	public static void onModelBake(ModelBakeEvent event) {
+		event.getModelRegistry().putObject(new ModelResourceLocation("skytechautosieve:auto_sieve", "inventory"),
+				new BakedModelAutoSieve());
+	}
+
 	@SubscribeEvent
 	public static void registerBlocks(RegistryEvent.Register<Block> event) {
 		event.getRegistry().registerAll(AUTO_SIEVE);
@@ -41,7 +53,6 @@ public class BlocksSubscriberHandler {
 	@SubscribeEvent
 	public static void registerModels(ModelRegistryEvent event) {
 		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityAutoSieve.class, new RenderBlockAutoSieve());
-		AUTO_SIEVE_ITEM.setTileEntityItemStackRenderer(new RenderItemAutoSieve());
 
 		ModelLoader.setCustomModelResourceLocation(AUTO_SIEVE_ITEM, 0,
 				new ModelResourceLocation("skytechautosieve:auto_sieve", "inventory"));
--- src/main/java/com/skytechautosieve/sieves/RenderItemAutoSieve.java
+++ src/main/java/com/skytechautosieve/sieves/RenderItemAutoSieve.java
@@ -1,8 +1,8 @@
 package com.skytechautosieve.sieves;
 
+import org.lwjgl.opengl.GL11;
+
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.client.renderer.tileentity.TileEntityItemStackRenderer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
@@ -13,21 +13,48 @@ public class RenderItemAutoSieve extends TileEntityItemStackRenderer {
 
 	@Override
 	public void renderByItem(ItemStack stack) {
-		TextureManager textureManager = Minecraft.getMinecraft().getTextureManager();
+		Minecraft.getMinecraft().getTextureManager().bindTexture(TEXTURE);
 
-		System.out.println("TEST");
-		textureManager.bindTexture(TEXTURE);
+		GL11.glPushMatrix();
 
-		GlStateManager.pushMatrix();
-		GlStateManager.enableBlend();
-		GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+		// Activation des fonctionnalités de rendu pour assurer la transparence
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		GL11.glDisable(GL11.GL_LIGHTING);
+		GL11.glEnable(GL11.GL_ALPHA_TEST);
 
-		GlStateManager.scale(1.0F, 1.0F, 1.0F);
-		GlStateManager.translate(0.0F, 0.0F, 0.0F);
+		// Facteur de réduction ajusté
+		float scaleFactor = 0.1F; // Réduction à 10% de la taille d'origine (128x128 -> 12.8x12.8)
+		GL11.glScalef(scaleFactor, scaleFactor, scaleFactor);
 
-		Minecraft.getMinecraft().ingameGUI.drawTexturedModalRect(0, 0, 0, 0, 16, 16);
+		// Ajustement fin du centrage (en tenant compte du facteur de réduction)
+		float translateX = (16.0F / scaleFactor - 16.0F) / 2.0F;
+		float translateY = (16.0F / scaleFactor - 16.0F) / 2.0F;
+		GL11.glTranslatef(-translateX + 74.0f, -translateY + 64.0f, 0.0F);
 
-		GlStateManager.disableBlend();
-		GlStateManager.popMatrix();
+		// Dessiner la texture 16x16 pixels
+		GL11.glBegin(GL11.GL_QUADS);
+
+		GL11.glTexCoord2f(0.0F, 1.0F); // Haut gauche (inversé)
+		GL11.glVertex2f(0, 0);
+
+		GL11.glTexCoord2f(1.0F, 1.0F); // Haut droit (inversé)
+		GL11.glVertex2f(16, 0);
+
+		GL11.glTexCoord2f(1.0F, 0.0F); // Bas droit (inversé)
+		GL11.glVertex2f(16, 16);
+
+		GL11.glTexCoord2f(0.0F, 0.0F); // Bas gauche (inversé)
+		GL11.glVertex2f(0, 16);
+
+		GL11.glEnd();
+
+		// Désactivation des fonctionnalités de rendu pour éviter les conflits
+		GL11.glDisable(GL11.GL_BLEND);
+		GL11.glDisable(GL11.GL_ALPHA_TEST);
+		GL11.glEnable(GL11.GL_LIGHTING);
+
+		GL11.glPopMatrix();
 	}
 }
