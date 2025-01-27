1308122420b2084891086dcfdb5c954bc1ba6afc
Hugo Piat <h.piat@outlook.fr>
2025-01-27 22:00:39 +0100

    Refactoring again :) (InternalTools to be removed)

--- src/main/java/com/skytechautosieve/utils/InternalTools.java
+++ src/main/java/com/skytechautosieve/utils/InternalTools.java
@@ -1,5 +1,10 @@
 package com.skytechautosieve.utils;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+
 public abstract class InternalTools {
 
 	public static void waitForTicks(Runnable callback, int ticks) {
@@ -11,4 +16,19 @@ public abstract class InternalTools {
 			}
 		}
 	}
+
+	public static boolean isPlayerAdmin(EntityPlayer player) {
+		if (Minecraft.getMinecraft().isSingleplayer()) {
+			return true;
+		}
+		if (player == null) {
+			return false;
+		}
+		MinecraftServer server = FMLCommonHandler.instance().getMinecraftServerInstance();
+		if (server != null) {
+			return server.getPlayerList().canSendCommands(player.getGameProfile());
+		}
+		return false;
+	}
+
 }
