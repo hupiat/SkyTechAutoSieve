3cf32b822d2e91c951174cc8c195f820bc0b493c
Hugo Piat <h.piat@outlook.fr>
2025-01-29 19:46:26 +0100

    Fixed server crash

--- src/main/java/com/skytechautosieve/BlocksSubscriberHandler.java
+++ src/main/java/com/skytechautosieve/BlocksSubscriberHandler.java
@@ -3,41 +3,20 @@ package com.skytechautosieve;
 import com.skytechautosieve.items.ItemFortuneUpgrade;
 import com.skytechautosieve.items.ItemSpeedUpgrade;
 import com.skytechautosieve.sieves.BlockAutoSieve;
-import com.skytechautosieve.sieves.TileEntityAutoSieve;
-import com.skytechautosieve.sieves.models.BakedModelAutoSieve;
-import com.skytechautosieve.sieves.renders.RenderBlockAutoSieve;
-import com.skytechautosieve.sieves.renders.RenderItemAutoSieve;
 
 import net.minecraft.block.Block;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
-import net.minecraftforge.client.event.ModelBakeEvent;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
-@Mod.EventBusSubscriber
+@Mod.EventBusSubscriber(modid = "skytechautosieve")
 public class BlocksSubscriberHandler {
 	public static final BlockAutoSieve AUTO_SIEVE = new BlockAutoSieve();
 	public static final Item AUTO_SIEVE_ITEM = new ItemBlock(AUTO_SIEVE).setRegistryName(AUTO_SIEVE.getRegistryName());
 	public static final Item SPEED_UPGRADE = new ItemSpeedUpgrade();
 	public static final Item FORTUNE_UPGRADE = new ItemFortuneUpgrade();
-
-	static {
-		AUTO_SIEVE_ITEM.setTileEntityItemStackRenderer(new RenderItemAutoSieve());
-	}
-
-	@SubscribeEvent
-	public static void onModelBake(ModelBakeEvent event) {
-		event.getModelRegistry().putObject(new ModelResourceLocation("skytechautosieve:auto_sieve", "inventory"),
-				new BakedModelAutoSieve());
-	}
 
 	@SubscribeEvent
 	public static void registerBlocks(RegistryEvent.Register<Block> event) {
@@ -47,18 +26,5 @@ public class BlocksSubscriberHandler {
 	@SubscribeEvent
 	public static void registerItems(RegistryEvent.Register<Item> event) {
 		event.getRegistry().registerAll(AUTO_SIEVE_ITEM, SPEED_UPGRADE, FORTUNE_UPGRADE);
-	}
-
-	@SideOnly(Side.CLIENT)
-	@SubscribeEvent
-	public static void registerModels(ModelRegistryEvent event) {
-		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityAutoSieve.class, new RenderBlockAutoSieve());
-
-		ModelLoader.setCustomModelResourceLocation(AUTO_SIEVE_ITEM, 0,
-				new ModelResourceLocation("skytechautosieve:auto_sieve", "inventory"));
-		ModelLoader.setCustomModelResourceLocation(SPEED_UPGRADE, 0,
-				new ModelResourceLocation("skytechautosieve:speed_upgrade", "inventory"));
-		ModelLoader.setCustomModelResourceLocation(FORTUNE_UPGRADE, 0,
-				new ModelResourceLocation("skytechautosieve:fortune_upgrade", "inventory"));
 	}
 }
---
+++ src/main/java/com/skytechautosieve/ClientEventsHandler.java
@@ -0,0 +1,42 @@
+package com.skytechautosieve;
+
+import com.skytechautosieve.sieves.TileEntityAutoSieve;
+import com.skytechautosieve.sieves.models.BakedModelAutoSieve;
+import com.skytechautosieve.sieves.renders.RenderBlockAutoSieve;
+import com.skytechautosieve.sieves.renders.RenderItemAutoSieve;
+
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraftforge.client.event.ModelBakeEvent;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
+@Mod.EventBusSubscriber(modid = "skytechautosieve", value = Side.CLIENT)
+public class ClientEventsHandler {
+	static {
+		BlocksSubscriberHandler.AUTO_SIEVE_ITEM.setTileEntityItemStackRenderer(new RenderItemAutoSieve());
+	}
+
+	@SubscribeEvent
+	public static void onModelBake(ModelBakeEvent event) {
+		event.getModelRegistry().putObject(new ModelResourceLocation("skytechautosieve:auto_sieve", "inventory"),
+				new BakedModelAutoSieve());
+	}
+
+	@SubscribeEvent
+	public static void registerModels(ModelRegistryEvent event) {
+		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityAutoSieve.class, new RenderBlockAutoSieve());
+
+		ModelLoader.setCustomModelResourceLocation(BlocksSubscriberHandler.AUTO_SIEVE_ITEM, 0,
+				new ModelResourceLocation("skytechautosieve:auto_sieve", "inventory"));
+		ModelLoader.setCustomModelResourceLocation(BlocksSubscriberHandler.SPEED_UPGRADE, 0,
+				new ModelResourceLocation("skytechautosieve:speed_upgrade", "inventory"));
+		ModelLoader.setCustomModelResourceLocation(BlocksSubscriberHandler.FORTUNE_UPGRADE, 0,
+				new ModelResourceLocation("skytechautosieve:fortune_upgrade", "inventory"));
+	}
+}
